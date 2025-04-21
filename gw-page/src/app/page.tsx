680922618543da49d2a52f650a7b5dea79e668c7
Stanislaw Malewicz <szeliga71@wp.pl>
2025-04-21 23:00:01 +0200

    first two points from Trello

--- gw-page/src/app/page.tsx
+++ gw-page/src/app/page.tsx
@@ -6,7 +6,7 @@ import Footer from "./component/footer";
 import Slider from "./component/slider1";
 import Carousel2 from "./component/carousel2";
 import Header from "./component/header";
-import UslugiJakosc from "./component/uslugiJakosc";
+import UslugiJakosc from "./component/designerskieMeble";
 import NowosciSection from "./component/nowosciSection";
 import BbItaliaSection from "./component/bbItaliaSection";
 import MarkiSection from "./component/markiSection";
@@ -14,6 +14,7 @@ import JakiegoStyluSection from "./component/jakiegoStyluSection";
 import CzyChceszZapytacSection from "./component/czyCheszZapytacSection";
 import PoradaExpertaSection from "./component/poradaExpertaSection";
 import MainLayout from "./layouts/MainLayout";
+import DesignerskieMeble from "./component/designerskieMeble";
 
 export default function Home() {
 
@@ -22,7 +23,7 @@ export default function Home() {
       <Header />
       <div className="overflow-y-auto overflow-x-hidden h-screen pt-[120px]">
         <Slider />
-        <UslugiJakosc />
+        <DesignerskieMeble />
         <NowosciSection />
         <BbItaliaSection />
         <Carousel2 />
